c76b8dfdd76a9bec9544036794c3807decf70a00
TheEnderCore <theendercore@gmail.com>
2024-11-16 16:50:39 +0200

    item init

--- src/main/java/com/theendercore/eliptic/Eliptic.java
+++ src/main/java/com/theendercore/eliptic/Eliptic.java
@@ -1,24 +1,22 @@
 package com.theendercore.eliptic;
 
+import com.theendercore.eliptic.init.ELPItems;
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Eliptic implements ModInitializer {
-	public static final String MOD_ID = "eliptic";
+    public static final String MOD_ID = "eliptic";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    public static Identifier id(String name) {
+        return Identifier.of(MOD_ID, name);
+    }
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        ELPItems.init();
+    }
 }
---
+++ src/main/java/com/theendercore/eliptic/init/ELPItems.java
@@ -0,0 +1,28 @@
+package com.theendercore.eliptic.init;
+
+import com.theendercore.eliptic.item.GlintItem;
+import net.minecraft.item.Item;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+
+import java.util.function.Function;
+
+import static com.theendercore.eliptic.Eliptic.id;
+
+public class ELPItems {
+    public static final Item ECHO_CORE_FRAGMENT = register("echo_core_fragment", glint(it -> it.rarity(Rarity.UNCOMMON)));
+
+    public static final Item ECHO_CORE = register("echo_core", glint(it -> it.rarity(Rarity.RARE)));
+    public static final Item ECHO_TRIDENT = register("echo_trident", new Item(new Item.Settings()));
+
+    public static void init() {
+    }
+
+    public static Item register(String name, Item item) {
+        return Registry.register(Registry.ITEM, id(name), item);
+    }
+
+    public static Item glint(Function<Item.Settings, Item.Settings> c) {
+        return new GlintItem(c.apply(new Item.Settings()));
+    }
+}
