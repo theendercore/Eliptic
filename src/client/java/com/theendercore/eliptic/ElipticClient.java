a00beb15aab87cfefa3b52fbc8123d32667da1fe
TheEnderCore <theendercore@gmail.com>
2024-11-16 15:00:48 +0200

    init

---
+++ src/client/java/com/theendercore/eliptic/ElipticClient.java
@@ -0,0 +1,10 @@
+package com.theendercore.eliptic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElipticClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
