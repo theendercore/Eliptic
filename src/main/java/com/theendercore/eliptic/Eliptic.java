96da2a598696c01d921e09859016dd278c154059
TheEnderCore <theendercore@gmail.com>
2024-11-21 15:39:51 +0200

    - attack events

--- src/main/java/com/theendercore/eliptic/Eliptic.java
+++ src/main/java/com/theendercore/eliptic/Eliptic.java
@@ -1,7 +1,14 @@
 package com.theendercore.eliptic;
 
-import com.theendercore.eliptic.init.*;
+import com.theendercore.eliptic.init.ELPConfig;
+import com.theendercore.eliptic.init.ELPItems;
+import com.theendercore.eliptic.init.ELPLootInjection;
+import com.theendercore.eliptic.init.ELPStatusEffects;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraftforge.api.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
@@ -24,5 +31,14 @@ public class Eliptic implements ModInitializer {
         ELPLootInjection.init();
         ELPStatusEffects.init();
 
+        PlayerBlockBreakEvents.BEFORE.register((world, player, pos, state, entity) -> !player.hasStatusEffect(ELPStatusEffects.ECHO));
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+            if (player.hasStatusEffect(ELPStatusEffects.ECHO)) return ActionResult.FAIL;
+            else return ActionResult.PASS;
+        });
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            if (player.hasStatusEffect(ELPStatusEffects.ECHO)) return ActionResult.FAIL;
+            else return ActionResult.PASS;
+        });
     }
 }
